d215b26c19f1c1a50cff09372620b82f3e8348cb
Raphael Regnier <raphael.regnier@hotmail.fr>
2018-06-17 20:09:23 +0200

    done redux for flats but still need to debug

---
+++ src/containers/flat_list.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setFlats } from '../actions';
+
+import Flat from '../components/flat';
+
+
+class FlatList extends Component {
+
+  // TEMPORARY CODE TO INTEGRATE HTML
+  // static defaultProps = {
+  //   flats: [{
+  //     "name": "Charm at the Steps of Montmartre",
+  //     "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+  //     "price": 164,
+  //     "priceCurrency": "EUR"
+  //   }]
+  // }
+
+  componentWillMount() {
+    this.props.setFlats();
+  }
+
+  render () {
+    return(
+      <div className="flat-list col-sm-7">
+        {this.props.flats.map((flat) =>  <Flat flat={flat} key={flat.name} /> )}
+      </div>
+    );
+  }
+}
+
+// function to wire the action to the props of the component
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setFlats: setFlats },
+    dispatch
+  );
+}
+
+// actually mapREDUXstatetoprops
+function mapReduxStateToProps(reduxState) {
+  return {
+    flats: reduxState.flats
+  };
+}
+
+export default connect(mapReduxStateToProps, mapDispatchToProps)(FlatList);
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = props => <div>Hello, {props.name}</div>;
+import flatsReducer from './reducers/flats_reducer';
+const reducers = combineReducers({
+  flats: flatsReducer
+});
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+
+import App from './components/app';
+
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<App class="container-fluid" />, root);
+// }
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  root);
